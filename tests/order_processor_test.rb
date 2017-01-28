a95049da4adacae9fc35a8fdabe0cb7b245d4afe
Joseph Hunt <josephb.hunt@gmail.com>
2017-01-28 00:21:27 -0500

    Removed unneeded order processor.

--- tests/order_processor_test.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require_relative '../order_processor'
-
-class OrderProcessorTest < Minitest::Test
-  def test_initialize_creates_an_order
-    order_processor = OrderProcessor.new
-    assert order_processor.order.is_a? Order
-  end
-end
